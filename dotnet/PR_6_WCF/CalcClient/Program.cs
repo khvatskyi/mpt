18fc68c32c314d12f2752b985ed07c9d8f974b93
Oleksandr Slobodian <55383070+khvatskyi@users.noreply.github.com>
2022-12-11 16:10:42 +0200

    removed redundant files and added pr6 (#4)
    Co-authored-by: oleksslob <slobodian.oleksandr.d@chnu.edu.ua>

---
+++ dotnet/PR_6_WCF/CalcClient/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ServiceModel;
+using CalcInterfaces;
+
+namespace CalcClient
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            ChannelFactory<ICalcService> channel = new ChannelFactory<ICalcService>("CalcServiceEndpoint");
+
+            ICalcService proxy = channel.CreateChannel();
+
+            Console.WriteLine($"Sum is {proxy.Sum(4, 2)}");
+            Console.WriteLine($"Subtraction is {proxy.Subtract(4, 2)}");
+            Console.WriteLine($"Multiply result is {proxy.Multiply(4, 2)}");
+            Console.WriteLine($"Divide is {proxy.Divide(4, 2)}");
+            Console.WriteLine($"Power is {proxy.Power(4, 2)}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ dotnet/PR_6_WCF/CalcServer/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ServiceModel;
+
+namespace CalcServer
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            using (ServiceHost host = new ServiceHost(typeof(CalcService)))
+            {
+                host.Open();
+                Console.WriteLine("Server started");
+                Console.WriteLine("Press <Enter> to close!!!");
+                Console.ReadLine();
+            }
+        }
+    }
+}
