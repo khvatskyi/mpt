1569f9de129366a602d789b5e9cb12242aaa8f77
oleksslob <slobodian.oleksandr.d@chnu.edu.ua>
2022-12-11 16:06:50 +0200

    pr6

---
+++ dotnet/PR_6_WCF/CalcInterfaces/ICalcService.cs
@@ -0,0 +1,23 @@
+﻿using System.ServiceModel;
+
+namespace CalcInterfaces
+{
+    [ServiceContract]
+    public interface ICalcService
+    {
+        [OperationContract]
+        int Sum(int firstNumber, int secondNumber);
+
+        [OperationContract]
+        int Subtract(int firstNumber, int secondNumber);
+
+        [OperationContract]
+        int Multiply(int firstNumber, int secondNumber);
+
+        [OperationContract]
+        int Divide(int numerator, int denominator);
+
+        [OperationContract]
+        int Power(int number, int power);
+    }
+}
---
+++ dotnet/PR_6_WCF/CalcServer/CalcService.cs
@@ -0,0 +1,37 @@
+﻿using CalcInterfaces;
+
+namespace CalcServer
+{
+    public class CalcService : ICalcService
+    {
+        public int Sum(int firstNumber, int secondNumber)
+        {
+            return firstNumber + secondNumber;
+        }
+
+        public int Subtract(int firstNumber, int secondNumber)
+        {
+            return firstNumber - secondNumber;
+        }
+
+        public int Multiply(int firstNumber, int secondNumber)
+        {
+            return firstNumber * secondNumber;
+        }
+
+        public int Divide(int numerator, int denominator)
+        {
+            return numerator / denominator;
+        }
+
+        public int Power(int number, int power)
+        {
+            if (power == 0)
+            {
+                return 1;
+            }
+
+            return number * Power(number, power - 1);
+        }
+    }
+}
